eb2f81044fb1ab6ce3ac91e2c98e0d08efc27e79
liligong <lydonlee@gmail.com>
2018-08-19 20:48:14 +0800

    add wechat main

--- handle.py
+++ handle.py
@@ -16,9 +16,10 @@ class Handle(object):
             nonce = data.nonce
             echostr = data.echostr
             token = "tokenweilu" #请按照公众平台官网\基本配置中信息填写
-            print("handle/GET func: signature: ,token",signature,token)
+            print("handle/GET func: signature: ,token ,echostr,timestamp",signature,token,echostr,timestamp)
 
             if not check_signature(token, signature, timestamp, nonce):  # 检查验证请求的签名
+                print("check fail")
                 return ""
             else:
                 return echostr
